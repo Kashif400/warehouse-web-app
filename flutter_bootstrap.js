e59b17e7a8fd80f5f4fd04f1ce46fb9c5dab934e
Kashif Shah <kashif900900@gmail.com>
2024-08-28 14:29:00 +0500

    add new code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3848831356"
+    serviceWorkerVersion: "557693766"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -60,14 +60,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "71d03ae23ab22c3bcbc492848f839eef
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "33f83c8c5cb0e9ae60b5f5674e18049e",
+"flutter_bootstrap.js": "ae5ddfed00a97fac64c08cebc19a3ebd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1d96e58d3be95a8228ccf6f916bbb525",
-"/": "1d96e58d3be95a8228ccf6f916bbb525",
-"main.dart.js": "ce0626d17ac18620940bc0113feb9657",
+"index.html": "d2b7c3e3533e25a98bb835b4c68c4c4b",
+"/": "d2b7c3e3533e25a98bb835b4c68c4c4b",
+"main.dart.js": "90df5309e17d6be7797175b0c5055a19",
 "manifest.json": "4b20fe75179100de58cba989c9a2d547",
 "version.json": "0fdec62a50d3415ad47345fb5926d309"};
 // The application shell files that are downloaded before a service worker can
